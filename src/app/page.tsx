d09dbc0e2a2db4948045f98c792886519514a170
GHeeJeon <jhjeon_99@naver.com>
2023-07-05 18:45:43 +0900

    ts-update js to ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,173 @@
+'use client' // 리액스 새 버전부터 서버 컴포넌트 도입(서버 렌더링(바로 서버 리소스 접근) 가능) 클라이언트에서 사용하는 useState, useEffect, useRef 는 불가능,
+             // 따라서 현재 컴포넌트를 클라이언트 컴포넌트로 바꾼다!
+
 import Image from 'next/image'
+import './globals.css';
+import { useState, useEffect, useRef } from 'react';
 
 export default function Home() {
+    const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+
+    interface DiaryEntry {
+        id: number;
+        date: string;
+        text: string;
+    }
+
+    const createDiary = (date: string, text: string) : DiaryEntry => ({
+        id: Date.now(),
+        date,
+        text,
+    });
+
+    const addDiary = (date: string, text: string) => {
+        const newDiary = createDiary(date, text);
+        setDiaries([...diaries, newDiary]);
+    };
+
+    const today = new Date(); // YYYY-MM-DD 형식을 padStart() 로 맞추자!
+    const dateToday = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+
+    const [dateInput, setDateInput] = useState(dateToday);
+    const [textInput, setTextInput] = useState('');
+
+    const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDateInput(event.target.value);
+    };
+
+    const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTextInput(event.target.value);
+    };
+
+    const [editingDiaryId, setEditingDiaryId] = useState<number | null>(null);
+    const [editDateInput, setEditDateInput] = useState('');
+    const [editTextInput, setEditTextInput] = useState('');
+
+    const handleEditDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEditDateInput(event.target.value);
+    };
+
+    const handleEditTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEditTextInput(event.target.value);
+    };
+
+    const editDiary = (id: number) => {
+        const diaryToEdit = diaries.find(diary => diary.id === id);
+        if (diaryToEdit) {  // find 메서드가 undefined 를 반환할 수 있으므로 체크
+            setEditDateInput(diaryToEdit.date);
+            setEditTextInput(diaryToEdit.text);
+        }
+        setEditingDiaryId(id);
+    };
+
+    const cancelEditing = () => {
+        setEditingDiaryId(null);
+    };
+
+    const updateDiary = (id: number | null, date: string, text: string) => {
+        if (id !== null) { // id가 null 이 아닌 경우 업데이트
+            setDiaries(diaries.map(diary =>
+                diary.id === id ? {...diary, date, text} : diary
+            ));
+            setEditingDiaryId(null);
+        }
+    };
+
+    const handleEditFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (editingDiaryId !== null && editDateInput && editTextInput) {
+            updateDiary(editingDiaryId, editDateInput, editTextInput);
+        }
+        if (snackbarTimeoutId.current) {
+            clearTimeout(snackbarTimeoutId.current);
+        }
+        setShowSnackbar('edit');
+        snackbarTimeoutId.current = setTimeout(() => {
+            setShowSnackbar('');
+        }, 2500);
+    };
+
+
+    const deleteDiary = (id: number) => {
+        setDiaries(diaries.filter(diary => diary.id !== id));
+    };
+
+    const [showSnackbar, setShowSnackbar] = useState('');
+    const snackbarTimeoutId = useRef<NodeJS.Timeout | null>(null);
+
+    useEffect(() => {
+        return () => {
+            if (snackbarTimeoutId.current) {
+                clearTimeout(snackbarTimeoutId.current);
+            }
+        };
+    }, []);
+
+    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("Hi!");
+        if (dateInput && textInput) {
+            addDiary(dateInput, textInput);
+            setDateInput(dateToday);
+            setTextInput('');
+            if (snackbarTimeoutId.current) {
+                clearTimeout(snackbarTimeoutId.current);
+            }
+            setShowSnackbar('success');
+        } else {
+            if (snackbarTimeoutId.current) {
+                clearTimeout(snackbarTimeoutId.current);
+            }
+            setShowSnackbar('error');
+        }
+        snackbarTimeoutId.current = setTimeout(() => {
+            setShowSnackbar('');
+        }, 2500);
+    };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <main className="App bg-blue-100 min-h-screen flex flex-col justify-center items-center space-y-5">
+        <div>
+          <h1 className="text-4xl text-blue-600">오늘의 일기</h1>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className="input-section border-solid border-2 border-sky-400 py-4 px-8 rounded-2xl">
+          <form className="input-form" onSubmit={handleFormSubmit}>
+            <input className="py-2 px-4 border-2 border-blue-400 rounded-md"  placeholder={'Enter date'} type="date" value={dateInput} onChange={handleDateChange}/>
+            <input className="py-2 px-4 border-2 border-blue-400 rounded-md" placeholder={'Enter text'} type="text" value={textInput} onChange={handleTextChange}/>
+            <button className="py-2 px-4 bg-blue-200 border-2 border-blue-300 rounded-md" type="submit">완료</button>
+          </form>
+          {showSnackbar === 'success' &&
+              <div className={`bg-green-500/75 snackbar ${showSnackbar ? 'show' : ''}`}>항목을 추가했어요!</div>}
+          {showSnackbar === 'edit' &&
+              <div className={`bg-blue-500/75 snackbar ${showSnackbar ? 'show' : ''}`}>항목을 수정했어요!</div>}
+          {showSnackbar === 'error' &&
+              <div className={`bg-red-500/75 snackbar ${showSnackbar ? 'show' : ''}`}>값을 입력해주세요!</div>}
+        </div>
+        <div className="list-section w-9/12">
+          {diaries.map(diary => (
+              <div key={diary.id} className="diary-item bg-transparent border-solid border-2 border-sky-300 m-4 py-4 px-8 rounded-2xl">
+                {editingDiaryId === diary.id ? (
+                    <>
+                      <div className="diary-date"><input className="py-1 px-2 border-2 border-blue-400 rounded-md" type="date" value={editDateInput} onChange={handleEditDateChange}/></div>
+                      <div className="diary-text"><input className="py-1 px-2 border-2 border-blue-400 rounded-md" type="text" value={editTextInput} onChange={handleEditTextChange}/></div>
+                      <div className="diary-action justify-between w-full">
+                        <button className="py-2 px-4 bg-blue-200 border-2 border-blue-300 rounded-md" type="submit" onClick={event => handleEditFormSubmit}>저장</button>
+                        <button className="py-2 px-4 bg-blue-200 border-2 border-blue-300 rounded-md" type="button" onClick={cancelEditing}>취소</button>
+                      </div>
+                    </>
+                ) : (
+                    <>
+                      <div className="diary-date">{diary.date}</div>
+                      <div className="diary-text">{diary.text}</div>
+                      <div className="diary-action justify-between w-full">
+                        <button className="py-1 px-2 bg-blue-200 border-2 border-blue-300 rounded-md" onClick={() => editDiary(diary.id)}>수정</button>
+                        <button className="py-1 px-2 bg-blue-200 border-2 border-blue-300 rounded-md" onClick={() => deleteDiary(diary.id)}>삭제</button>
+                      </div>
+                    </>
+                )}
+              </div>
+          ))}
+        </div>
+      </main>
   )
 }
